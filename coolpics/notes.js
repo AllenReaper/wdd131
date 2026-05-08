bd487932b43234fc2cde3666e0a14f2a63e794b8
Jaxson McCammon <jaxman00@gmail.com>
2026-05-08 14:06:42 -0600

    PART1 OF COOL PIZ

---
+++ coolpics/notes.js
@@ -0,0 +1,19 @@
+//ghdhg 
+
+//<span class="fs-exclude avatar " style="background-image: url(https://byui.instructure.com/images/thumbnails/164707726/Yrsig2hGA2yGCiqLAQYuClpt32WIB9Kbl0PzDSIw);width: 128px;height: 128px"><span class="screenreader-only">Joshua Walker</span></span>
+//        <img src="https://byui.instructure.com/images/thumbnails/164707726/Yrsig2hGA2yGCiqLAQYuClpt32WIB9Kbl0PzDSIw" alt="BYUI logo">
+//        <img src="https://byui.instructure.com/images/thumbnails/164707726/Yrsig2hGA2yGCiqLAQYuClpt32WIB9Kbl0PzDSIw" alt="JS, CSS, and HTML logos"></img></img>
+
+
+
+
+//nav a {
+        //padding: 200px;
+        //color: black;
+        //margin: 1em;
+        //display: contents;
+//    }
+//    nav a:hover {
+ //       
+ //       color: #616061;
+ //   }
