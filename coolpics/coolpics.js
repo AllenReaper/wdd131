b843f6a2d9d2885e375026e14220e1c881b5961d
Jaxson McCammon <jaxman00@gmail.com>
2026-05-15 14:53:23 -0600

    udate to cool josh

---
+++ coolpics/coolpics.js
@@ -0,0 +1,46 @@
+
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+let menuBtn = document.getElementsByClassName("menu-btn")[0];
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    console.log(e.target.src);
+    modalImage.src = e.target.src.replace("sm", "full");
+    modal.showModal();
+    
+// Code to show modal  - Use event parameter 'e'   
+    
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+        
+
+
+console.log(menuBtn);
+
+menuBtn.addEventListener("click", handleMenuBtnClick);
+
+function handleMenuBtnClick(event) {
+    let nav = document.querySelector("nav");
+    nav.classList.toggle("hide");
+    menuBtn.classList.toggle("change");
+
+};
+
+
+
+
